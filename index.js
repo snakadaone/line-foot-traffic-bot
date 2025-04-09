01580112bc72f9263efe2c766ddfb4ce3dd1f921
simon yang <simonyang@MacBook-Pro-2.local>
2025-04-10 03:07:20 +0800

    Fix missing comma in userState assignment

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/webhook', async (req, res) => {
       ...userState[userId],
       location: { lat: latitude, lng: longitude },
       city: cityOnly,
-      weather  
+      weather,  
       districtOnly,
     };
       
